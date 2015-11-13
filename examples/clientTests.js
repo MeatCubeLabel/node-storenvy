f1e237475401488c3d91a73ad528eca9741832cd
Ryan Lewis <ryanharrisonlewis@gmail.com>
2015-11-13 00:13:47 -0800

    just cleaning up stuff

--- examples/clientTests.js
+++ examples/clientTests.js
@@ -8,8 +8,8 @@ function goodRes(obj) {
 var tests = {
 	runAll: function(seClient) {
 		tests.seClient = seClient;
-		for(var func in tests) {
-			if(func !== 'runAll' && typeof tests[func] === 'function') {
+		for (var func in tests) {
+			if (func !== 'runAll' && typeof tests[func] === 'function') {
 				tests[func]();
 			}
 		}
@@ -19,11 +19,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getUserInfo(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.id)
+			if (goodRes(obj) &&
+				obj.data.id) {
 				console.log('User Info Test passed.');
-			else 
+			} else {
 				console.log('User Info Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -31,12 +32,13 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getStoreInfo(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
+			if (goodRes(obj) &&
 				obj.data.name &&
-				obj.data.url)
+				obj.data.url) {
 				console.log('Store Info Test passed.');
-			else 
+			} else {
 				console.log('Store Info Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -44,11 +46,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getStoreVisits(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.weekly_visits)
+			if (goodRes(obj) &&
+				obj.data.weekly_visits) {
 				console.log('Store Visits Test passed.');
-			else 
+			} else { 
 				console.log('Store Visits Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -56,11 +59,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getStoreTemplates(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.templates)
+			if (goodRes(obj) &&
+				obj.data.templates) {
 				console.log('Store Templates Test passed.');
-			else 
+			} else {
 				console.log('Store Templates Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -72,17 +76,17 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.createStoreTemplate(testName, { content: testContent },
 			function(err, data) {
-				if(err) {
+				if (err) {
 					return console.log('Create Store Template Failed: ' + err);
 				}
 				seClient.getStoreTemplates(function(err, data) {
 					var templates = JSON.parse(data).data.templates;
 					templates.forEach(function(val) {
-						if(val.name === testName) {
+						if (val.name === testName) {
 							foundTemplate = val;
 						}
 					});
-					if(!foundTemplate) {
+					if (!foundTemplate) {
 						return console.log('Create Store Template Failed, did not create template');
 					}
 					console.log('Create Store Template passed.');
@@ -94,11 +98,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getOrders(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.orders)
+			if (goodRes(obj) &&
+				obj.data.orders) {
 				console.log('Orders Test passed.');
-			else 
+			} else { 
 				console.log('Orders Test failed with ' + JSON.stringify(obj));
+			}
 
 			//call order test since we have some orders here
 			tests.orderTest(seClient, obj.data.orders[0].id);
@@ -106,15 +111,18 @@ var tests = {
 	},
 
 	orderTest: function(seClient, orderId) {
-		if(!orderId) return;
+		if (!orderId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getOrder(orderId, function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.id === orderId)
+			if (goodRes(obj) &&
+				obj.data.id === orderId) {
 				console.log('Order Test passed.');
-			else 
+			} else { 
 				console.log('Order Test failed with ' + JSON.stringify(obj));
+			}
 
 			//call order products test with our current order
 			tests.orderProductsTest(seClient, orderId);
@@ -122,15 +130,18 @@ var tests = {
 	},
 
 	orderProductsTest: function(seClient, orderId) {
-		if(!orderId) return;
+		if (!orderId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getOrderProducts(orderId, function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.products)
+			if (goodRes(obj) &&
+				obj.data.products) {
 				console.log('Order Products Test passed.');
-			else 
+			} else { 
 				console.log('Order Products Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -138,11 +149,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getProducts(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.products)
+			if (goodRes(obj) &&
+				obj.data.products) {
 				console.log('Products Test passed.');
-			else 
+			} else { 
 				console.log('Products Test failed with ' + JSON.stringify(obj));
+			}
 
 			//call productTest with an id
 			tests.productTest(seClient, obj.data.products[0].id);
@@ -150,15 +162,18 @@ var tests = {
 	},
 
 	productTest: function(seClient, productId) {
-		if(!productId) return;
+		if (!productId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getProduct(productId, function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.id === productId)
+			if (goodRes(obj) &&
+				obj.data.id === productId) {
 				console.log('Product Test passed.');
-			else 
+			} else { 
 				console.log('Product Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -170,8 +185,8 @@ var tests = {
 		seClient.createProduct(testName, testCents, { status: testStatus },
 			function(err, data) {
 				var newProduct = JSON.parse(data);
-				if(newProduct.data.name !== testName &&
-					newProduct.data.cents !== cents) {
+				if (newProduct.data.name !== testName &&
+					newProduct.data.cents !== testCents) {
 					console.log('Create Product Test failed with ' + JSON.stringify(newProduct));
 				} else {
 					seClient.deleteProduct(newProduct.data.id);
@@ -183,25 +198,27 @@ var tests = {
 	updateProductTest: function(seClient) {
 		var testName = 'the ultimatest beard shampoo',
 			testCents = '777',
-			testStatus = 'hidden';
+			testStatus = 'hidden',
+			updateCents = '666',
+			newProduct;
 		seClient = seClient || tests.seClient;
 		seClient.createProduct(testName, testCents, { status: testStatus },
 			function(err, data) {
-				if(err) {
+				if (err) {
 					return console.log('Update Product Test failed with ' + err);
 				}
-				var newProduct = JSON.parse(data);
-				seClient.updateProduct(newProduct.data.id, { cents: '666' }, function(err, data) {
-					if(err) {
+				seClient.updateProduct(newProduct.data.id, { cents: updateCents }, function(err, data) {
+					if (err) {
 						return console.log('Update Product Test failed with ' + err);
 					}
 					try {
-						var newProduct = JSON.parse(data);
+						newProduct = JSON.parse(data);
 					} catch(ex) {
 						return console.log('Update Product Test failed with ' + ex);
 					}
-					if(newProduct.data.name !== testName &&
-						newProduct.data.cents !== cents) {
+					if (newProduct &&
+						newProduct.data.name !== testName &&
+						newProduct.data.cents !== updateCents) {
 						console.log('Update Product Test failed with ' + JSON.stringify(newProduct));
 					} else {
 						seClient.deleteProduct(newProduct.data.id);
@@ -215,11 +232,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getCollections(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.collections)
+			if (goodRes(obj) &&
+				obj.data.collections) {
 				console.log('Collections Test passed.');
-			else 
+			} else { 
 				console.log('Collections Test failed with ' + JSON.stringify(obj));
+			}
 
 			//call collectionTest with an id
 			tests.collectionTest(seClient, obj.data.collections[0].id);
@@ -227,15 +245,18 @@ var tests = {
 	},
 
 	collectionTest: function(seClient, collectionId) {
-		if(!collectionId) return;
+		if (!collectionId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getCollection(collectionId, function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.id === collectionId)
+			if (goodRes(obj) &&
+				obj.data.id === collectionId) {
 				console.log('Collection Test passed.');
-			else 
+			} else { 
 				console.log('Collection Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -243,11 +264,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getShippingGroups(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.shipping_groups)
+			if (goodRes(obj) &&
+				obj.data.shipping_groups) {
 				console.log('Shipping Groups Test passed.');
-			else 
+			} else {
 				console.log('Shipping Groups Test failed with ' + JSON.stringify(obj));
+			}
 
 			//call shippingGroupTest with an id
 			tests.shippingGroupTest(seClient, obj.data.shipping_groups[0].id);
@@ -255,17 +277,19 @@ var tests = {
 	},
 
 	shippingGroupTest: function(seClient, groupId) {
-		if(!groupId) return;
+		if (!groupId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getShippingGroup(groupId, function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.id === groupId)
+			if (goodRes(obj) &&
+				obj.data.id === groupId) {
 				console.log('Shipping Group Test passed.');
-			else 
+			} else { 
 				console.log('Shipping Group Test failed with ' + JSON.stringify(obj));
+			}
 
-			//console.log(JSON.stringify(obj));
 			//need to call shippingRatesTest here since we have the id's
 			tests.shippingRatesTest(seClient, groupId, 
 				obj.data.shipping_rates[0].shipping_rate.shipping_class_id);
@@ -276,11 +300,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getShippingClasses(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.shipping_classes)
+			if (goodRes(obj) &&
+				obj.data.shipping_classes) {
 				console.log('Shipping Classes Test passed.');
-			else 
+			} else { 
 				console.log('Shipping Classes Test failed with ' + JSON.stringify(obj));
+			}
 
 			//call shippingClassTest with an id
 			tests.shippingClassTest(seClient, obj.data.shipping_classes[0].id);
@@ -288,29 +313,35 @@ var tests = {
 	},
 
 	shippingClassTest: function(seClient, classId) {
-		if(!classId) return;
+		if (!classId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getShippingClass(classId, function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.id === classId)
+			if (goodRes(obj) &&
+				obj.data.id === classId) {
 				console.log('Shipping Class Test passed.');
-			else 
+			} else { 
 				console.log('Shipping Class Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
 	shippingRatesTest: function(seClient, groupId, classId) {
-		if(!groupId && !classId) return;
+		if (!groupId && !classId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getShippingRate(groupId, classId, function(err, data) { 
 			var obj = JSON.parse(data);
 			if(goodRes(obj) &&
 				obj.data.first_item_in_cents &&
-				obj.data.shipping_group_id === groupId)
+				obj.data.shipping_group_id === groupId) {
 				console.log('Shipping Rates Test passed.');
-			else 
+			} else { 
 				console.log('Shipping Rates Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	},
 
@@ -318,11 +349,12 @@ var tests = {
 		seClient = seClient || tests.seClient;
 		seClient.getWebhooks(function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.webhooks)
+			if (goodRes(obj) &&
+				obj.data.webhooks) {
 				console.log('Webhooks Test passed.');
-			else 
+			} else { 
 				console.log('Webhooks Test failed with ' + JSON.stringify(obj));
+			}
 
 			//call shippingClassTest with an id
 			tests.webhookTest(seClient, obj.data.webhooks[0].id);
@@ -330,15 +362,18 @@ var tests = {
 	},
 
 	webhookTest: function(seClient, webhookId) {
-		if(!webhookId) return;
+		if (!webhookId) {
+			return;
+		}
 		seClient = seClient || tests.seClient;
 		seClient.getWebhook(webhookId, function(err, data) { 
 			var obj = JSON.parse(data);
-			if(goodRes(obj) &&
-				obj.data.id === webhookId)
+			if (goodRes(obj) &&
+				obj.data.id === webhookId) {
 				console.log('Webhook Test passed.');
-			else 
+			} else { 
 				console.log('Webhook Test failed with ' + JSON.stringify(obj));
+			}
 		});
 	}
 };
